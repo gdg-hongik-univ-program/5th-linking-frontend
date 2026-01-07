6f08deb5dd8efa61c9e68114a4714f36d4d8d0d1
yeop1108 <yeop1108@naver.com>
2026-01-08 02:29:43 +0900

    feat: 로그인 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from "react-router-dom"; // BrowserRouter 삭제
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import Layout from "./components/layout/Layout";
 
-const App = () => {
-  const [count, setCount] = useState(0)
-
+function App() {
   return (
-    <>
-      <div>
-       </div>
-    </>
-  )
+    <div className="min-h-screen bg-zinc-900 flex justify-center items-center">
+      <div className="w-full max-w-[390px] min-h-screen bg-brand-bg shadow-xl overflow-hidden relative border-x border-zinc-700">
+      <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route element={<Layout />}>
+        <Route path="/home" element={<HomePage />} />
+        </Route>
+      </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
