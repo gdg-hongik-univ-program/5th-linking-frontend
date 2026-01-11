63b588204efc5947c37be423c0d27993a3cb3e0f
seokyun9 <westse05@gmail.com>
2026-01-11 20:08:34 +0900

    feat: 회원가입 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"; // BrowserRouter 삭제
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./components/layout/Layout";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="w-full max-w-[390px] min-h-screen bg-brand-bg shadow-xl overflow-hidden relative border-x border-zinc-700">
       <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path = "/signup" element = {<SignupPage />} />
       <Route element={<Layout />}>
         <Route path="/home" element={<HomePage />} />
         </Route>
